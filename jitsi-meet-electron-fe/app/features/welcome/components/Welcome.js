97b40698a103f1790b891865a96620aefad16902
nirneu-infotale <nirn@infotale.com>
2025-04-22 17:41:02 +0300

    refactor: remove unused commented code from Welcome component

--- jitsi-meet-electron-fe/app/features/welcome/components/Welcome.js
+++ jitsi-meet-electron-fe/app/features/welcome/components/Welcome.js
@@ -263,52 +263,6 @@ class Welcome extends Component<Props, State> {
     );
   }
 
-  /**
-   * Renders the header for the welcome page.
-   *
-   * @returns {ReactElement}
-   */
-  // _renderHeader() {
-  //   const locationState = this.props.location.state;
-  //   const locationError = locationState && locationState.error;
-  //   const { t, isAuthenticated, user } = this.props;
-  //   return (
-  //     <Header>
-  //       <SpotlightTarget name="conference-url">
-  //         <Form onSubmit={this._onFormSubmit}>
-  //           {isAuthenticated && user && (
-  //             <div
-  //               style={{
-  //                 marginBottom: "15px",
-  //                 fontSize: "16px",
-  //                 color: "#000000",
-  //                 fontWeight: "bold",
-  //               }}>
-  //               {t("Welcome")}, {user.name}
-  //             </div>
-  //           )}
-  //           <Label>{t("enterConferenceNameOrUrl")} </Label>
-  //           <FieldWrapper>
-  //             <FieldTextStateless
-  //               autoFocus={true}
-  //               isInvalid={locationError}
-  //               isLabelHidden={true}
-  //               onChange={this._onURLChange}
-  //               placeholder={this.state.roomPlaceholder}
-  //               shouldFitContainer={true}
-  //               type="text"
-  //               value={this.state.url}
-  //             />
-  //             <Button appearance="primary" onClick={this._onJoin} type="button">
-  //               {t("go")}
-  //             </Button>
-  //           </FieldWrapper>
-  //         </Form>
-  //       </SpotlightTarget>
-  //     </Header>
-  //   );
-  // }
-
   _updateRoomname: () => void;
 
   /**
